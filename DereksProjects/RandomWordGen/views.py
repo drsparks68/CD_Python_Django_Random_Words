604f210ce53e45a2e2d28680151e5e22af4ccce5
despar <despar@microsoft.com>
2021-02-28 16:41:08 -0800

    first iteration

---
+++ DereksProjects/RandomWordGen/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirecct
+from django.utils.crypto import get_random_string
+
+def index(request):
+    if 'counter' not in request_session:
+        request.session['counter'] = 0
+    request.session['counter'] += 1
+    request.session['new_word'] = get_random_string(length=24)
+    return render(request, 'index.html')
+
+def reset(request):
+    request.session['counter'] = 0
+    return redirect('/random_word')
+# Create your views here.
